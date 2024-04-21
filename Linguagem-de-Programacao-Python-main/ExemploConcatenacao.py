7c83d28c64324eb578ad273d7fc688b8d411d8b0
Fábio borges <145725895+fabioborges456@users.noreply.github.com>
2024-04-21 19:48:41 -0300

    Add files via upload

---
+++ Linguagem-de-Programacao-Python-main/ExemploConcatenacao.py
@@ -0,0 +1,4 @@
+nomeincompleto = input("Digite o seu nome: ")
+sobrenome = input("Digite o seu sobrenome: ")
+nomecompleto= nomeincompleto + " " + sobrenome
+print("O nome digitado foi: {}".format(nomecompleto))
---
+++ Linguagem-de-Programacao-Python-main/README.md
@@ -0,0 +1,2 @@
+# Linguagem de Programacao Python
+Aulas de Lógica de Programação em Python Faculdade Anhanguera 1º Semestre de 2021
